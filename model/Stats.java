3ed0856cb1393eca7847a958aaff2a90d197f72b
Thomas <t.j.stein@student.utwente.nl>
2017-01-27 22:22:43 +0100

    Add initial stat class

---
+++ model/Stats.java
@@ -0,0 +1,108 @@
+package model;
+
+import java.util.Random;
+
+import model.exceptions.InvalidStatAmountException;
+import model.exceptions.InvalidStatRangeException;
+import model.exceptions.StatException;
+
+public class Stats {
+	public static final int STAT_COUNT = 6;
+	public static final int STAT_MAX = 100;
+	public static final int STAT_SUM = STAT_COUNT * 50;
+	
+	public Stats(int[] statArray) throws StatException {
+		if (statArray.length != STAT_COUNT) {
+			throw new InvalidStatAmountException();
+		}
+		
+		for (int stat: statArray) {
+			if (stat > STAT_MAX) {
+				throw new InvalidStatRangeException();
+			}
+		}
+		
+		this.hp = statArray[0];
+		this.atk = statArray[1];
+		this.def = statArray[2];
+		this.spatk = statArray[3];
+		this.spdef = statArray[4];
+		this.speed = statArray[5];
+	}
+	
+	public Stats(int hp, int atk, int def, int spatk, int spdef, int speed) {
+		this.hp = hp;
+		this.atk = atk;
+		this.def = def;
+		this.spatk = spatk;
+		this.spdef = spdef;
+		this.speed = speed;
+	}
+	
+	public int hp;
+	public int atk;
+	public int def;
+	public int spatk;
+	public int spdef;
+	public int speed;
+	
+	public int getTotal() {
+		return hp + atk + def + spatk + spdef + speed;
+	}
+	
+	@Override
+	public String toString() {
+		return "Stats [hp=" + hp + ", atk=" + atk + ", def=" + def + ", spatk=" + spatk + ", spdef=" + spdef
+				+ ", speed=" + speed + ", getTotal()=" + getTotal() + "]";
+	}
+	
+	public static Stats randomizeStats() {
+		Stats stats = null;
+		boolean found = false;
+		
+		Random r = new Random();
+		
+		while (!found) {
+			double[] doubles = new double[STAT_COUNT];
+			int[] ints = new int[STAT_COUNT];
+			
+			double total = 0;
+			
+			for (int i = 0; i < STAT_COUNT; i++) {
+				doubles[i] = r.nextDouble();
+				total += doubles[i];
+			}
+			
+			double factor = STAT_SUM / total;
+			
+			int sum = 0;
+			
+			for (int i = 0; i < STAT_COUNT; i++) {
+				double stat = doubles[i] * factor;
+				ints[i] = (int) Math.round(stat);
+				sum += ints[i];
+			}
+	
+			while (sum < STAT_SUM) {
+				ints[r.nextInt(6)]++;
+				sum++;
+			}
+			
+			while (sum > STAT_SUM) {
+				ints[r.nextInt(6)]--;
+				sum--;
+			}
+			
+			try {
+				stats = new Stats(ints);
+				found = true;
+			} catch (StatException e) {
+				// TODO Auto-generated catch block
+				//e.printStackTrace();
+			}
+			
+		}
+		
+		return stats;
+	}
+}
---
+++ model/exceptions/InvalidStatAmountException.java
@@ -0,0 +1,10 @@
+package model.exceptions;
+
+public class InvalidStatAmountException extends StatException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7483318396731504268L;
+
+}
