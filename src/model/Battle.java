f5b68819dae6937a6a13cd86ed670c47ddfe1c81
Thomas <t.j.stein@student.utwente.nl>
2017-01-28 17:09:24 +0100

    Add map loading to Battle constructor

--- src/model/Battle.java
+++ src/model/Battle.java
@@ -1,9 +1,12 @@
 package model;
 
+import java.io.IOException;
+
 public class Battle {
 	private Map map;
 	
-	public Battle() {
-		
+	public Battle(String mapName) throws IOException {
+		map = Map.loadMap(mapName);
+		System.out.println(map);
 	}
 }
