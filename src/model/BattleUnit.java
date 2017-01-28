1c0a143930d3f9d7cf6d688b03437c3ff4c54ef9
Thomas <t.j.stein@student.utwente.nl>
2017-01-28 18:28:12 +0100

    Add static unit parser method

--- src/model/BattleUnit.java
+++ src/model/BattleUnit.java
@@ -1,13 +1,16 @@
 package model;
 
+import model.exceptions.StatException;
+
 public class BattleUnit {
 	private Unit unit;
 	
 	private int xPos;
 	private int yPos;
 
-	public BattleUnit(Unit unit) {
+	public BattleUnit(Unit unit, int x, int y) {
 		this.unit = unit;
+		setPosition(x, y);
 	}
 	
 	public void setPosition(int x, int y) {
@@ -15,4 +18,55 @@ public class BattleUnit {
 		yPos = y;
 	}
 
+	
+	public static BattleUnit parseBattleUnit(String data) {
+		Stats stats = null;
+		int x = -1;
+		int y = -1;
+		
+		String[] args = data.split(" ");
+		for (int i = 0; i <  args.length; i++) {
+			String[] arg = args[i].split("=");
+			
+			switch (arg[0]) {
+			case "stats":
+				if (arg[1].equals("random")) {
+					stats = Stats.randomizeStats();
+				} else {
+					String[] statStrings = arg[1].split(",");
+					int[] statArray = new int[statStrings.length];
+					
+					try {
+						for (int j = 0; j < statStrings.length; j++) {
+							statArray[i] = Integer.parseInt(statStrings[i]);
+						}
+					} catch (NumberFormatException e) {
+						e.printStackTrace();
+					}
+
+					
+					try {
+						stats = new Stats(statArray);
+					} catch (StatException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+				}
+				break;
+				
+			case "x":
+				x = Integer.parseInt(arg[1]);
+				break;
+				
+			case "y":
+				y = Integer.parseInt(arg[1]);
+				break;
+			}
+		}
+		
+		Unit unit = new Unit(stats);
+		BattleUnit bUnit = new BattleUnit(unit, x, y);
+		
+		return bUnit;
+	}
 }
