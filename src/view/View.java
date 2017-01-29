8f5d326d0e744fd2e9479886c1f75a95681eadea
Thomas <t.j.stein@student.utwente.nl>
2017-01-29 02:13:18 +0100

    Allow View to regain focus from textfield

--- src/view/View.java
+++ src/view/View.java
@@ -6,6 +6,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.util.ArrayList;
 
 import javax.swing.JFrame;
@@ -43,11 +45,42 @@ public class View extends JFrame implements Runnable, KeyListener {
 		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
-		
+		this.addMouseListener(new MouseListener() {
+
+			@Override
+			public void mouseClicked(MouseEvent event) {
+				event.getComponent().requestFocus();
+			}
+
+			@Override
+			public void mouseEntered(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseExited(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mousePressed(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+
+			@Override
+			public void mouseReleased(MouseEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+		});
 		
 		addKeyListener(this);
         setFocusable(true);
-        setFocusTraversalKeysEnabled(false);
+        //setFocusTraversalKeysEnabled(false);
         
         //setSize(new Dimension(height, width));
       	pack();
